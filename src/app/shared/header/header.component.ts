9bf7297f8a69b44cf625557aee0b8469c1d61bb2
joaolp-gomes <joao.lp.gomes@gmail.com>
2024-09-28 17:33:21 +0100

    (fix) Missing module

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  templateUrl: "./header.component.html",
+  imports: [RouterModule],
+  styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
   private readonly _router = inject(Router);
 
   navigateToCreatePost(): void {
-    this._router.navigate(['/create']);
+    this._router.navigate(["/create"]);
   }
 }
